620a797e2a1f281b7850fc0d7fd65ecff88affb9
vyacheslav sykhanov <nogamegolife@gmail.com>
2016-06-28 21:35:34 +0400

    add some new proj

---
+++ work-assets/dist/js/function.js
@@ -0,0 +1,9 @@
+!(function() {
+
+  $('.flexslider').flexslider({
+    animation: "slide",
+    controlsContainer: $(".custom-controls-container"),
+    customDirectionNav: $(".custom-navigation a")
+  });
+
+})();
